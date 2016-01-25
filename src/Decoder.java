9de41b69f6ae5c390f425b21a27cf6684588e6d7
brianbabcockapps <brian@brianbabcockapps.com>
2016-01-24 19:22:13 -0800

    change from mergesort to quicksort

--- src/Decoder.java
+++ src/Decoder.java
@@ -27,129 +27,83 @@ public class Decoder {
 			}
 		}
 		
-		//reduce the array to make sorting faster
-		letters = reduce(letters);
-		
-		letters = sort(letters);
+		//SortArray
+		quickSort(letters);
 		
 		//print letters
 		for (int i = 0; i < letters.length; i++) {
+			if (letters[i].getLetter() == '_') {
+				break;
+			}
 			System.out.print(letters[i].getLetter());
 		}
 	}
 	
 	/**
-	 * Reduce the array to only letters to sort in order to make sorting faster
+	 * Takes in array and split it by pivot
+	 * 
 	 * @param letters
+	 * @param l for left most of array
+	 * @param r for right most of array
 	 * @return
 	 */
-	public static LetterBean[] reduce (LetterBean [] letters)
-	{
-		LetterBean [] temp;
-		int count  = 0;
-		int j = 0;
+	public static int partition (LetterBean [] letters, int l, int r) {
 		
-		//Get size of new array
-		for (int i = 0; i < letters.length; i++){
-			if (letters[i].getCount() > letters[UNDERSCORE].getCount()) {
-				count++;
+		int left = l, right = r;
+		int pivot = letters[(left + right)/2].getCount(); 
+		
+		while (left <= right) {
+			while (letters[left].getCount() > pivot) {
+				left++;
+			}
+			while (letters[right].getCount() < pivot) {
+				right--;
+			}
+			
+			if (left <= right) {
+				LetterBean temp = letters[left];
+				letters[left++] = letters[right];
+				letters[right--] = temp;
 			}
 		}
-		
-		//Make the smaller array.
-		temp = new LetterBean[count];
-		
-		for (int i = 0; i < count; i++){
-			while (letters[j].getCount() < letters[UNDERSCORE].getCount())  {
-				j++;
-			}
-			temp[i] = letters[j++];
-		}
-		
-		return temp;
-	}
-
-	/**
-	 * This function sets up the merge sort recusive function
-	 * @param letters
-	 * @return
-	 */
-	public static LetterBean [] sort (LetterBean [] letters)
-	{
-		int arrayLength = letters.length - 1;
-		int middle = arrayLength/2;
-	    mergeSort(letters, 0, middle);
-	    mergeSort(letters, middle + 1, arrayLength);
-	    merge(letters, 0, middle, arrayLength);
-		
-		return letters;
+		return left;
 	}
 	
 	/**
-	 * This recursive function splits up the array into smaller parts
+	 * Initialize quicksort.  Set left and right and only sort the letters that matter.
+	 * 
 	 * @param letters
-	 * @param begin
-	 * @param end
-	 * @return
 	 */
-	public static LetterBean [] mergeSort(LetterBean [] letters, int begin, int end)
-	{
-		if (begin < end) {
-			int middle = begin + (end - begin)/2;
-			mergeSort(letters, begin, middle);
-			mergeSort(letters, middle + 1, end);
-			merge(letters, begin, middle, end);
-		}
-		return letters;
+	public static void quickSort (LetterBean [] letters) {
+		int left = 0, right = UNDERSCORE;
+		
+		LetterBean temp = letters[(left + right)/2];
+		letters[(left + right)/2] = letters[right];
+		letters[right] = temp;
+		
+		int index = partition (letters, left, right);
+		
+	    if (left < index - 1) {
+	    	quickSort(letters, left, index - 1);
+	    }
 	}
 	
 	/**
-	 * Merges and sorts the divided array into 
+	 * Main quickSort recursion function.  Keep partitioning and pivot.
+	 * 
 	 * @param letters
-	 * @param begin
-	 * @param middle
-	 * @param end
-	 * @return
+	 * @param left
+	 * @param right
 	 */
-	public static LetterBean [] merge (LetterBean [] letters, int begin, int middle, int end)
-	{
-		LetterBean temp [] = new LetterBean [end - begin + 1];
+	public static void quickSort (LetterBean [] letters, int left, int right) {
 		
-		int i = 0;
-		int j = 0;
-		int k = 0;
+		int index = partition (letters, left, right);
 		
-		while ((i <= middle - begin) && (j < end - middle)) {
-			if (letters[begin + i].getCount() > letters[middle + 1 + j].getCount()) {
-				temp[k] = letters[begin + i];
-				k++;
-				i++;
-			}
-			else
-			{
-				temp[k] = letters[middle + 1 + j];
-				k++;
-				j++;
-			}
-		}
-		
-		while (i <= middle - begin) {
-			temp[k] = letters[begin + i];
-			k++;
-			i++;
-		}
-		
-		while (j < end - middle) {
-			temp[k] = letters[middle + 1 + j];
-			k++;
-			j++;
-		}
-		
-		k = 0;
-		for (i = begin; i <= end; i ++) {
-			letters[i] = temp[k++];
-		}
-		
-		return letters;
+	    if (left < index - 1) {
+	    	quickSort(letters, left, index - 1);
+	    }
+	    if (index < right) {
+	    	quickSort(letters, index, right);
+	    }
 	}
 }
